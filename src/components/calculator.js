cd3ba3dcfe65d0b865292689edeeac7b7151c712
leloufadel <leloufadel@gmail.com>
2023-07-13 15:17:59 +0300

    calculate event

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import Output from './output';
+import calculate from '../logic/calculate';
 
-const Calculator = () => {
-  const btnValues = ['AC', '+/-', '%', '\u00F7', '7', '8', '9',
-    'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+const btnValues = ['AC', '+/-', '%', '\u00F7', '7', '8', '9',
+  'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+function Calculator() {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+
+  const handleClick = (btnValue) => {
+    setState(calculate(state, btnValue));
+  };
 
   return (
     <div className="calculator-grid">
-      <Output />
+      <Output next={state.next} operation={state.operation} total={state.total} />
       {btnValues.map((value) => {
         if (value === '0') {
           return (
-            <button key={value} className="span-two" type="button">
+            <button key={value} className="span-two" type="button" onClick={() => handleClick(value)}>
               {value}
             </button>
           );
         } if (value === '=' || value === '+' || value === '-' || value === 'x' || value === '\u00F7') {
           return (
-            <button key={value} className="orange-button" type="button">
+            <button key={value} className="orange-button" type="button" onClick={() => handleClick(value)}>
               {value}
             </button>
           );
         }
         return (
-          <button key={value} type="button">
+          <button key={value} type="button" onClick={() => handleClick(value)}>
             {value}
           </button>
         );
       })}
     </div>
   );
-};
+}
 
 export default Calculator;
--- src/components/output.js
+++ src/components/output.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './calculator.css';
 
-const Output = () => (
+const Output = ({ next, operation, total }) => (
   <div className="output">
-    0
+    {next || operation || total || 0}
     <div className="previous-operand" />
     <div className="current-operand" />
   </div>
 );
+
+Output.propTypes = {
+  next: PropTypes.string,
+  operation: PropTypes.string,
+  total: PropTypes.string,
+};
+
+Output.defaultProps = {
+  next: '',
+  operation: '',
+  total: '',
+};
+
 export default Output;
